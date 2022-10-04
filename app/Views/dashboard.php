9f1e289e530b54774f1497e68130b17e6cec5ef8
nadilazumitia <nadilzputri25@gmail.com>
2022-10-04 16:39:17 +0700

    dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -3,39 +3,49 @@
 <?= $this->section('content') ?>
 
 <div>
-    <h1></h1>
-    <h3></h3>
+    <h1>Selamat Datang..</h1>
+    <h3>Di Sistem Informasi Inventaris Barang SMPIT Iqra' Kota Bengkulu</h3>
 </div>
 
 <div>
     <div>
-        <div class="judul"></div>
-        <div class="gambar"></div>
-        <div class="detail"></div>
+        <div class="Jumlah Gedung"></div>
+        <div class="gambar">
+        <i class="fa-solid fa-school"></i>
+        </div>
+        <div class="view details"></div>
     </div>
 
     <div>
-        <div class="judul"></div>
-        <div class="gambar"></div>
-        <div class="detail"></div>
+        <div class="Jumlah Ruangan"></div>
+        <div class="gambar">
+        <i class="fa-solid fa-door-open"></i>
+        </div>
+        <div class="view details"></div>
     </div>
 
     <div>
-        <div class="judul"></div>
-        <div class="gambar"></div>
-        <div class="detail"></div>
+        <div class="Jumlah Barang"></div>
+        <div class="gambar">
+        <i class="fa-solid fa-layer-group"></i>
+        </div>
+        <div class="view details"></div>
     </div>
 
     <div>
-        <div class="judul"></div>
-        <div class="gambar"></div>
-        <div class="detail"></div>
+        <div class="Pembelian Barang"></div>
+        <div class="gambar">
+        <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+        <div class="view details"></div>
     </div>
 
     <div>
-        <div class="judul"></div>
-        <div class="gambar"></div>
-        <div class="detail"></div>
+        <div class="Laporan"></div>
+        <div class="gambar">
+        <i class="fa-regular fa-file"></i>
+        </div>
+        <div class="view details"></div>
     </div>
 </div>
 
