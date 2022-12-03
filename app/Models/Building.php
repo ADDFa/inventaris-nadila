7048125a1a95c3ddaabaa46de16b201ce059a210
ADDFa <fratamaadha18@gmail.com>
2022-12-03 17:42:23 +0700

    set model

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -6,37 +6,14 @@ use CodeIgniter\Model;
 
 class Building extends Model
 {
-    protected $DBGroup          = 'default';
     protected $table            = 'buildings';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $returnType       = 'object';
+    protected $allowedFields    = [
+        'user_id',
+        'gedung_name',
+        'bulding_size',
+        'total_room',
+        'building_registration_date',
+        'building_image'
+    ];
 }
